a545528fada6eb7261dbc5e033983f271129ed11
Engineer <kawacukent@gmail.com>
2026-05-19 21:48:27 +0200

    fix: add eslint explicit dep, add solc 0.8.24 to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,14 +9,22 @@ dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.20',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
+    compilers: [
+      {
+        version: '0.8.20',
+        settings: {
+          optimizer: { enabled: true, runs: 200 },
+          viaIR: true,
+        },
       },
-      viaIR: true,
-    },
+      {
+        version: '0.8.24',
+        settings: {
+          optimizer: { enabled: true, runs: 200 },
+          viaIR: true,
+        },
+      },
+    ],
   },
   networks: {
     hardhat: {
